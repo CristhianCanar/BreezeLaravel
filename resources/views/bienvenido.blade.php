bb4ce7207768861a936475189b9372cb371e19d1
CristhianCanar <cristhian.canar.m@gmail.com>
2024-11-13 13:10:45 -0500

    Commit relacionado con el inicio de sesion

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'Bienvenido desde el title')
+
+@section('contenido')
+    <h1>Hola a todos</h1>
+    <p>Nuevo parrafo</p>
+@endsection
+
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title', 'Contacto')
+
+@section('contenido')
+    <h1>Contenido</h1>
+    <p>Puede contactarse al +57111111111</p>
+@endsection
